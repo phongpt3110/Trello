784441a7f2d1c942d379d77d24580be8c2b61a1b
windy <ptp3110@gmail.com>
2025-03-31 01:07:56 +0700

    Bổ sung

--- trello_api/src/controllers/boardController.js
+++ trello_api/src/controllers/boardController.js
@@ -5,17 +5,25 @@ import { boardService } from '~/services/boardService'
 
 // Tạo ra một controller để xử lý logic, trung chuyển dữ liệu - nhận request từ client
 const createNew = async (req, res, next) => {
-
   try {
     // Điều hướng dữ liệu qua tầng service / input nhận giá trị FE
     const createdBoard = await boardService.createNew(req.body)
-
     // Kết quả trả về phía client
     res.status(StatusCodes.CREATED).json(createdBoard)
     // throw new ApiError(StatusCodes.BAD_REQUEST, 'Windydev test error') test lỗi middleware
-
   } catch (err) {
-
+    // res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: new Error(err).message })
+    next(err)} // Chuyển hướng đến middleware xử lý lỗi
+}
+// Tạo ra một controller để xử lý logic, trung chuyển dữ liệu - nhận request từ client
+const createNew = async (req, res, next) => {
+  try {
+    // Điều hướng dữ liệu qua tầng service / input nhận giá trị FE
+    const createdBoard = await boardService.createNew(req.body)
+    // Kết quả trả về phía client
+    res.status(StatusCodes.CREATED).json(createdBoard)
+    // throw new ApiError(StatusCodes.BAD_REQUEST, 'Windydev test error') test lỗi middleware
+  } catch (err) {
     // res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: new Error(err).message })
     next(err)} // Chuyển hướng đến middleware xử lý lỗi
 }
